ae5eb4f8c816e8314fc6d9ea82961df91502cc32
jorgedipra <jorgedipra@gmail.com>
2024-04-09 10:36:31 -0500

    instalación manual de Analítica 4

--- src/views/SeccionHome.vue
+++ src/views/SeccionHome.vue
@@ -18,6 +18,21 @@ export default {
     CompHome,
     CompContat,
     CompPorta
+  },
+  mounted() {
+    // Declarar la variable dataLayer
+    window.dataLayer = window.dataLayer || [];
+
+    // Google tag (gtag.js)
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-S1V7MGTZDB');
+
+    var script = document.createElement('script');
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-S1V7MGTZDB";
+    script.async = true;
+    document.body.appendChild(script);
+    //AddToAny END
   }
 }
 </script>
