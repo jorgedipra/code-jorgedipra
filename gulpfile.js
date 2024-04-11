96718bd5038f8483ab99fce695c6a9df728ae66b
jorgedipra <jorgedipra@gmail.com>
2024-04-11 17:13:18 -0500

    Se desarrolló Gulp como una herramienta para automatizar procesos en el desarrollo de proyectos. Se definieron tres procesos específicos: 'run-prod', 'edit-index' y 'move-files'.

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const replace = require('gulp-replace');
+const { exec } = require('child_process');
+const rename = require('gulp-rename');
+
+
+// Tarea para ejecutar npm run prod
+gulp.task('run-prod', function (cb) {
+    exec('npm run prod', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.error(stderr);
+        cb(err);
+    });
+});
+
+// Tarea para editar el archivo index.html
+gulp.task('edit-index', function () {
+    return gulp.src('./dist/index.html') // Ruta al archivo index.html
+        .pipe(replace(/<title>.*<\/title>/g, function (match) {
+            return match + '\n<meta name="description" content="Explora mi web personal para obtener información detallada sobre mi experiencia, habilidades y proyectos pasados. Descubre cómo puedo ayudarte en tu próximo proyecto. ¡Contáctame hoy para empezar!">';
+        }))
+        .pipe(gulp.dest('./dist/')); // Ruta de destino del archivo editado
+});
+
+// Tarea para mover archivos existentes de ./dist/* a ../page/*
+gulp.task('move-files', function () {
+    return gulp.src('./dist/**/*') // Ruta de origen
+        .pipe(rename(function (path) {
+            // Cambia el directorio base a ../page/
+            path.dirname = '../page/' + path.dirname;
+        }))
+        .pipe(gulp.dest('./')); // Ruta de destino
+});
+
+// Tarea predeterminada que ejecuta las tareas edit-index y run-prod en secuencia
+gulp.task('default', gulp.series(
+    'run-prod',
+    'edit-index',
+    'move-files'
+));
--- src/App.vue
+++ src/App.vue
@@ -18,20 +18,6 @@ export default {
         }
     },
     mounted() {
-      // // Espera a que el DOM esté completamente cargado
-      // document.addEventListener("DOMContentLoaded", function() {
-      //     // Obtén el elemento <head> del documento
-      //     var head = document.querySelector("head");
-
-      //     // Crea un elemento <meta> para la descripción
-      //     var metaDescription = document.createElement("meta");
-      //     metaDescription.setAttribute("name", "description");
-      //     metaDescription.setAttribute("content", "Explora mi web personal para obtener información sobre mi experiencia, habilidades y proyectos.¡Contáctame hoy para empezar!");
-
-      //     // Agrega la meta descripción al elemento <head>
-      //     head.appendChild(metaDescription);
-      // });
-
     },
   components: {
     CompHeader,
