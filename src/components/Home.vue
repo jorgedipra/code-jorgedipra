532be98bdf72a07b5d6a074bf147711fb084c266
Cerlalc Admin <jdiaz@cerlalc.org>
2026-05-21 14:39:31 -0500

    🧹 chore(ui): remove legacy components replaced by new design
    - Remove Home.vue (old home component - replaced by new sections)
    - Remove header.vue (replaced by Navbar.vue)
    - Remove footer.vue (replaced by FooterSection.vue)
    - Remove views/SeccionContacto.vue (replaced by component version)
    - Format SeccionPortafolio.vue with consistent styling

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,21 +2,24 @@
   <div class="card">
     <div class="card-image">
       <figure class="image banner">
-        <img src="../assets/baner.png">
+        <img src="../assets/baner.png" />
       </figure>
     </div>
     <div class="card-content">
       <div class="media">
         <div class="media-left">
           <figure class="image is-64x64">
-            <img class="is-rounded" src="../assets/avatar.png" alt="Placeholder image">
+            <img class="is-rounded" src="../assets/avatar.png" alt="Placeholder image" />
           </figure>
         </div>
         <div class="media-content">
           <h1 class="title is-4">Jorge Diaz</h1>
           <h2 class="subtitle is-6">@jorgedipra</h2>
-          <div class="a2a_kit a2a_kit_size_32 a2a_default_style" data-a2a-url="https://jorgedipra.github.io/"
-            data-a2a-title="jorgedipra">
+          <div
+            class="a2a_kit a2a_kit_size_32 a2a_default_style"
+            data-a2a-url="https://jorgedipra.github.io/"
+            data-a2a-title="jorgedipra"
+          >
             <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
             <a class="a2a_button_facebook"></a>
             <a class="a2a_button_linkedin"></a>
@@ -26,12 +29,18 @@
         </div>
         <p class="is-4">
           <b-icon pack="fas" icon="briefcase" type="is-navis-danger"></b-icon>
-          Desarrollador Master en Cerlalc- Unesco. <br>
+          Desarrollador Master en Cerlalc- Unesco. <br />
           <b-icon pack="fas" icon="street-view" type="is-navis-danger"></b-icon>
-          Bogotá, Colombia<br>
+          Bogotá, Colombia<br />
           <span class="is-right">
-            <b-button type="is-dark" size="is-medium" tag="a" href="https://github.com/jorgedipra/jorgedipra.github.io"
-              target="_blank" expanded>
+            <b-button
+              type="is-dark"
+              size="is-medium"
+              tag="a"
+              href="https://github.com/jorgedipra/jorgedipra.github.io"
+              target="_blank"
+              expanded
+            >
               <span class="icon-text">
                 <span class="icon">
                   <b-icon pack="fab" icon="github" type="is-navis-danger"></b-icon>
@@ -40,20 +49,18 @@
               </span>
             </b-button>
           </span>
-
         </p>
       </div>
-      <hr>
+      <hr />
       <div class="content">
         <div class="card">
           <div class="card-content">
             <p class="title">
-              “La mayoría de la gente piensa que el diseño es una chapa, es una simple decoración. Para mí, nada es más
-              importante en el futuro que el diseño. El diseño es el alma de todo lo creado por el hombre.”
+              “La mayoría de la gente piensa que el diseño es una chapa, es una simple decoración.
+              Para mí, nada es más importante en el futuro que el diseño. El diseño es el alma de
+              todo lo creado por el hombre.”
             </p>
-            <p class="subtitle">
-              Steve Jobs
-            </p>
+            <p class="subtitle">Steve Jobs</p>
           </div>
           <footer class="card-footer">
             <p class="card-footer-item">
@@ -63,7 +70,8 @@
             </p>
             <p class="card-footer-item">
               <span>
-                Contact on <a href="https://www.linkedin.com/in/jorgedipra/" target="_blank">Linkedin</a>
+                Contact on
+                <a href="https://www.linkedin.com/in/jorgedipra/" target="_blank">Linkedin</a>
               </span>
             </p>
             <p class="card-footer-item">
@@ -73,9 +81,9 @@
             </p>
           </footer>
         </div>
-        <br>Desarrollada en: <a>@bulmaio</a>, <a>@vuejs</a>,
-        <a href="#">#css</a> <a href="#">#responsive</a>
-        <br>
+        <br />Desarrollada en: <a>@bulmaio</a>, <a>@vuejs</a>, <a href="#">#css</a>
+        <a href="#">#responsive</a>
+        <br />
         <time datetime="2016-1-1">Actualizado: 11:09 PM - 19 Marzo 2024</time>
       </div>
     </div>
@@ -88,14 +96,14 @@ export default {
   mounted() {
     //AddToAny BEGIN
     var a2a_config = a2a_config || {};
-    a2a_config.locale = "es";
+    a2a_config.locale = 'es';
     var script = document.createElement('script');
-    script.src = "https://static.addtoany.com/menu/page.js";
+    script.src = 'https://static.addtoany.com/menu/page.js';
     script.async = true;
     document.body.appendChild(script);
     //AddToAny END
   }
-}
+};
 </script>
 
 <style scoped>
@@ -104,6 +112,6 @@ a {
 }
 
 .image img.is-rounded {
-  background: #2196F3;
+  background: #2196f3;
 }
 </style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,126 +1,130 @@
 <template>
-    <b-navbar type="is-dark">
-        <template #brand>
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <img src="../assets/logo.png" alt="Jorgedipra">
-                <p class="title txt-white">Jorgedipra</p>
-            </b-navbar-item>
-        </template>
-        <template #start>
-            <b-navbar-item href="https://jorgedipra.blogspot.com/" target="_blank">
-                <span class="icon-text">
-                    <span class="icon">
-                        <b-icon pack="fab" icon="blogger-b" type="is-navis-danger"></b-icon>
-                    </span>
-                    <span>Blog</span>
-                </span>
-            </b-navbar-item>
-            <b-navbar-item href="#" id="portafolioLink">
-                <span class="icon-text">
-                    <span class="icon">
-                        <b-icon pack="fas" icon="folder" type="is-navis-danger"></b-icon>
-                    </span>
-                    <span>Portafolio</span>
-                </span>
-            </b-navbar-item>
-            <b-navbar-item href="https://drive.google.com/drive/folders/0ByWJai73PqMpeE1MTGZqZnJVbnc" target="_blank">
-                <span class="icon-text">
-                    <span class="icon">
-                        <b-icon pack="fas" icon="certificate" type="is-navis-danger"></b-icon>
-                    </span>
-                    <span>Certificados</span>
-                </span>
-            </b-navbar-item>
-            <b-navbar-item href="#" id="contactoLink">
-                <span class="icon-text">
-                    <span class="icon">
-                        <b-icon pack="fas" icon="envelope" type="is-navis-danger"></b-icon>
-                    </span>
-                    <span>Contáctame</span>
-                </span>
-            </b-navbar-item>
-        </template>
-
-        <template #end>
-            <b-navbar-item tag="div">
-                <div class="buttons">
-                    <a class="button is-light">
-                        Log in
-                    </a>
-                </div>
-            </b-navbar-item>
-        </template>
-    </b-navbar>
-    <button @click="goToTop" id="goToTopBtn" title="Ir Arriba">
-        <span class="icon is-medium">
-            <i class="fas fa-arrow-up fa-2x"></i>
+  <b-navbar type="is-dark">
+    <template #brand>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img src="../assets/logo.png" alt="Jorgedipra" />
+        <p class="title txt-white">Jorgedipra</p>
+      </b-navbar-item>
+    </template>
+    <template #start>
+      <b-navbar-item href="https://jorgedipra.blogspot.com/" target="_blank">
+        <span class="icon-text">
+          <span class="icon">
+            <b-icon pack="fab" icon="blogger-b" type="is-navis-danger"></b-icon>
+          </span>
+          <span>Blog</span>
         </span>
-    </button>
+      </b-navbar-item>
+      <b-navbar-item href="#" id="portafolioLink">
+        <span class="icon-text">
+          <span class="icon">
+            <b-icon pack="fas" icon="folder" type="is-navis-danger"></b-icon>
+          </span>
+          <span>Portafolio</span>
+        </span>
+      </b-navbar-item>
+      <b-navbar-item
+        href="https://drive.google.com/drive/folders/0ByWJai73PqMpeE1MTGZqZnJVbnc"
+        target="_blank"
+      >
+        <span class="icon-text">
+          <span class="icon">
+            <b-icon pack="fas" icon="certificate" type="is-navis-danger"></b-icon>
+          </span>
+          <span>Certificados</span>
+        </span>
+      </b-navbar-item>
+      <b-navbar-item href="#" id="contactoLink">
+        <span class="icon-text">
+          <span class="icon">
+            <b-icon pack="fas" icon="envelope" type="is-navis-danger"></b-icon>
+          </span>
+          <span>Contáctame</span>
+        </span>
+      </b-navbar-item>
+    </template>
+
+    <template #end>
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <a class="button is-light"> Log in </a>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+  <button @click="goToTop" id="goToTopBtn" title="Ir Arriba">
+    <span class="icon is-medium">
+      <i class="fas fa-arrow-up fa-2x"></i>
+    </span>
+  </button>
 </template>
 
 <script>
 export default {
-    name: 'CompHeader',
-    mounted() {
-        window.onscroll = function () { scrollFunction() };
+  name: 'CompHeader',
+  mounted() {
+    window.onscroll = function () {
+      scrollFunction();
+    };
 
-        function scrollFunction() {
-            var goToTopBtn = document.getElementById("goToTopBtn");
+    function scrollFunction() {
+      var goToTopBtn = document.getElementById('goToTopBtn');
 
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                goToTopBtn.style.display = "block";
-            } else {
-                goToTopBtn.style.display = "none";
-            }
-        }
-
-
-    },
-    methods: {
-        goToTop() {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-        }
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        goToTopBtn.style.display = 'block';
+      } else {
+        goToTopBtn.style.display = 'none';
+      }
     }
-}
+  },
+  methods: {
+    goToTop() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 a {
-    color: #428cc7;
+  color: #428cc7;
 }
 
 .txt-white,
 .link-home {
-    color: #fff;
+  color: #fff;
 }
 
 nav {
-    border-radius: 0 0 5px 5px;
-    box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
-    padding: 5px 20px;
+  border-radius: 0 0 5px 5px;
+  box-shadow:
+    0 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12);
+  padding: 5px 20px;
 }
 
 span.icon.has-text-navis-danger {
-    color: #E91E63;
+  color: #e91e63;
 }
 
 button#goToTopBtn {
-    display: none;
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    background-color: #E91E63;
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    padding: 10px 20px;
-    cursor: pointer;
-    z-index: 99;
+  display: none;
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  background-color: #e91e63;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+  z-index: 99;
 }
 
 button#goToTopBtn:hover {
-    background-color: #8bc34a;
+  background-color: #8bc34a;
 }
 </style>
